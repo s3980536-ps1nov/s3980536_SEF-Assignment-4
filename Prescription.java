9b4c2e1955714f569336a4f9fb55e40d49305c6e
s3980536-ps1nov <s3980536@student.rmit.edu.au>
2024-10-16 14:24:17 +1100

    base skeleton code

---
+++ Prescription.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Prescription{
+    private int prescID;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private float sphere;
+    private float axis;
+    private float cylinder;
+    private Date examinationDate;
+    private String optometrist;
+    private String[] remarkTypes= {"Client", "Optometrist"};
+    private ArrayList <String> postRemarks= new ArrayList<>();
+
+    public boolean addPrescription() {
+        return true;
+    }
+
+    public boolean addRemark() {
+        return true;
+    }
+
+}
